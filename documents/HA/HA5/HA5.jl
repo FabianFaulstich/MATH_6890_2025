965f40e7f61a2a400397acec2415f8b67ee04c75
Fabian Faulstich <fabianfaulstich@AttaBoy.lan>
2025-03-21 21:48:19 -0400

    HA5

---
+++ documents/HA/HA5/HA5.jl
@@ -0,0 +1,149 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9dcf67f4-0657-11f0-0519-e7a230009de3
+md"# Homework assignment
+Due Date: 03/31 by 11:59 pm
+
+Fill in your solutions in the Pluto notebook provided below. Once completed, execute the notebook and export it as a PDF. Upload the PDF to Gradescope for grading, and assign the pages to the respective exercises. Please ensure that all of your solutions, including the code you wrote, are visible and legible in the exported PDF before submitting it to Gradescope. Keep in mind that adjustments to assignments after the submission deadline will not be accommodated.
+
+"
+
+# ╔═╡ 0a4714b9-3bd3-484e-903e-49284b53d30e
+md"# Research Proposal - Part I"
+
+# ╔═╡ 6c1e9e6e-cdda-475f-bc55-b6d81e250f6a
+md" 
+In this assignment, you will begin developing a one-page research proposal. Your goal is to identify a topic of scientific interest and merit, and to ground your proposal in the methods we've explored in class—such as Hartree–Fock theory, Green’s functions, configuration interaction, Rayleigh–Schrödinger and Møller–Plesset perturbation theory. You are also encouraged to consider methods we will cover later in the course, such as coupled cluster theory, as well as techniques beyond our syllabus, including density functional theory (DFT), density matrix embedding theory (DMET), dynamical mean-field theory (DMFT), and auxiliary-field quantum Monte Carlo (AFQMC), among others. Begin by conducting a literature review to understand the current state of research in your chosen area. Then, select a topic that genuinely interests you and has the potential for scientific contribution. 
+"
+
+# ╔═╡ 66617216-6d70-42df-a714-580e353f40b6
+md" #### Exercise 1: Identify and Justify a Research Topic
+"
+
+# ╔═╡ 69fe7536-825a-445b-9bb7-f7c3a6c98433
+md"
+a. Explore areas of interest: From the methods we have discussed, as well as those not covered in class, choose one research area that interests you.
+"
+
+# ╔═╡ d97b1b85-ef96-45e7-963d-4354825aaf88
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 251d8e20-5b33-4b4e-91ef-f29157ffad43
+md"
+b. Identify a gap or question: Within that area, find a specific question or gap in current knowledge that you would like to explore.
+"
+
+# ╔═╡ 3f458393-a0a7-48f0-b0b0-4e67c4cd690f
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ b099f248-bc6c-4de3-ad91-7873114e9b24
+md"
+c. Write a short paragraph explaining why this topic is scientifically valuable. Be sure to mention how it connects to the broader fields of quantum chemistry or condensed matter physics and why it merits further study.
+"
+
+# ╔═╡ 714e212c-6deb-4d3d-b884-de9b93770b26
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 339f35d0-0eec-4b5f-a78f-87a397816705
+md" #### Exercise 2: Conduct a Focused Literature Review
+"
+
+# ╔═╡ f6baacae-14d8-4eed-aac6-c0046963c254
+md"
+a. Gather at least five relevant references (journal articles, reviews, or seminal papers) on your chosen topic. Make sure they are reputable sources in the field.
+"
+
+# ╔═╡ 5cb422ac-2cf8-4ac8-98fa-211c5fbe6637
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 5d6a1487-436e-411a-9d27-5b26a176bfd0
+md"
+b. Briefly summarize each reference: For each of the five, provide one or two sentences about their key findings or contributions.
+"
+
+# ╔═╡ 6ffaa44a-00e6-4319-9c27-6e7e18574b29
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 4a62b88d-3ac5-4c6d-bc25-5d63b37d141e
+md"
+c. Highlight the connection to your chosen research question: Explain how each paper informs or supports your topic, either by pointing to an open question, providing a theoretical or methodological framework, or offering data relevant to your intended proposal.
+"
+
+# ╔═╡ 5f79f3ee-2f91-4efb-8d92-ed23e723a4f9
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ d2c9405f-cb4e-4f9a-98d2-78fe702da540
+md" #### Exercise 3: Draft a Preliminary Proposal Outline
+"
+
+# ╔═╡ ac66e052-0b4c-4c5c-8e3a-1d57832bb1fb
+md"
+a. State your research question or hypothesis in one or two concise sentences.
+"
+
+# ╔═╡ c13799ea-601e-4077-96ba-8491f131740d
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 38c412eb-6cf7-4bbe-b0be-2ded14680fae
+md"
+b. Discuss the methods, approaches, or techniques you plan to use. Explain why these methods are suitable and what advantages or challenges they may present.
+"
+
+# ╔═╡ 82fdc8bf-50b3-46d7-80e0-5d58a747148d
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ 1236853a-3d54-4627-9bde-2259a053b573
+md"
+c. Propose potential outcomes or impacts: In a brief paragraph, describe what new insights or contributions your project could provide to the field.
+"
+
+# ╔═╡ c557c368-6900-4a38-932c-96f600c3647d
+md"
+Your answer goes here ...
+"
+
+# ╔═╡ Cell order:
+# ╟─9dcf67f4-0657-11f0-0519-e7a230009de3
+# ╟─0a4714b9-3bd3-484e-903e-49284b53d30e
+# ╟─6c1e9e6e-cdda-475f-bc55-b6d81e250f6a
+# ╟─66617216-6d70-42df-a714-580e353f40b6
+# ╟─69fe7536-825a-445b-9bb7-f7c3a6c98433
+# ╟─d97b1b85-ef96-45e7-963d-4354825aaf88
+# ╟─251d8e20-5b33-4b4e-91ef-f29157ffad43
+# ╟─3f458393-a0a7-48f0-b0b0-4e67c4cd690f
+# ╟─b099f248-bc6c-4de3-ad91-7873114e9b24
+# ╟─714e212c-6deb-4d3d-b884-de9b93770b26
+# ╟─339f35d0-0eec-4b5f-a78f-87a397816705
+# ╟─f6baacae-14d8-4eed-aac6-c0046963c254
+# ╟─5cb422ac-2cf8-4ac8-98fa-211c5fbe6637
+# ╟─5d6a1487-436e-411a-9d27-5b26a176bfd0
+# ╟─6ffaa44a-00e6-4319-9c27-6e7e18574b29
+# ╟─4a62b88d-3ac5-4c6d-bc25-5d63b37d141e
+# ╟─5f79f3ee-2f91-4efb-8d92-ed23e723a4f9
+# ╟─d2c9405f-cb4e-4f9a-98d2-78fe702da540
+# ╟─ac66e052-0b4c-4c5c-8e3a-1d57832bb1fb
+# ╟─c13799ea-601e-4077-96ba-8491f131740d
+# ╟─38c412eb-6cf7-4bbe-b0be-2ded14680fae
+# ╟─82fdc8bf-50b3-46d7-80e0-5d58a747148d
+# ╟─1236853a-3d54-4627-9bde-2259a053b573
+# ╟─c557c368-6900-4a38-932c-96f600c3647d
